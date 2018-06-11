e0f2aa40428a6b34ac003f9dfd2be7748ef8a84b
Matej Đuroković <matejdurokovic@gmail.com>
2018-06-11 10:04:42 +0200

    New project, added models

---
+++ adoptions/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Pet(models.model):
+	SEX_CHOICES= [('M', 'Male'), ('F', 'Female')]
+	name = models.CharField(max_length=100)
+	submitter = models.CharField(max_length=100)
+	species = models.CharField(max_length=30)
+	breed = models.CharField(max_length=30, blank=True)
+	description = models.TextField()
+	sex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
+	submission_date = models.DateTimeField()
+	age = models.IntegerField(null=True)
+	vaccinations = models.ManyToMany('Vaccine', blank=True)
+
+
+class Vaccine(models.model):
+	name = models.CharField(max_length=50)
